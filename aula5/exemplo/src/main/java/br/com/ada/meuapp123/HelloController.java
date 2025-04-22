581d43594fbc35de6a382cb64fa3427b5861e042
Marcel Tanuri <marcel.tanuri@gmail.com>
2025-04-22 20:35:01 -0300

    add aula5

---
+++ aula5/exemplo/src/main/java/br/com/ada/meuapp123/HelloController.java
@@ -0,0 +1,14 @@
+package br.com.ada.meuapp123;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class HelloController {
+
+    @GetMapping
+    public String hello(){
+        return "Hello";
+    }
+
+}
